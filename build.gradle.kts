7f8a52b79fcbfb1a8c76a0dc3478f329be3db6b5
Rafael Correia <rafaeljpc@gmail.com>
2024-09-26 02:39:48 -0300

    add liquibase scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+import org.liquibase.gradle.LiquibaseTask
 import java.io.IOException
 import java.util.*
 
 val javaVersion = JavaVersion.VERSION_21
-val srcMainDbPath = "${projectDir.absolutePath}/src/main/db"
+val srcMainDbPath = file("${projectDir.absolutePath}/src/main/db")
 
 group = "io.github.soat7"
 version = "0.0.1-SNAPSHOT"
@@ -39,6 +40,14 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    main {
+        resources {
+            srcDirs + srcMainDbPath
+        }
+    }
+}
+
 dependencies {
     implementation("info.picocli:picocli:4.+")
     implementation("org.liquibase:liquibase-core:4.+")
@@ -48,29 +57,37 @@ dependencies {
     liquibaseRuntime("info.picocli:picocli:4.+")
     liquibaseRuntime("org.liquibase:liquibase-core:4.+")
     liquibaseRuntime("org.postgresql:postgresql:42.7.+")
+    liquibaseRuntime(sourceSets.main.get().resources)
 }
 
+
 liquibase {
-    activities.register("update") {
+    this.activities.register("update") {
         this.arguments = mapOf(
             "changelogFile" to "/changelog/master.xml",
-            "searchPath" to srcMainDbPath,
             "url" to props["DATABASE_URL"],
             "username" to props["DATABASE_USER"],
             "password" to props["DATABASE_PASSWORD"],
             "logLevel" to "debug"
         )
-    }
-    activities.register("rollback") {
-        this.arguments = mapOf(
-            "changelogFile" to "$srcMainDbPath/changelog/master.xml",
-            "url" to "jdbc:${props["DATABASE_URL"]}",
-            "username" to props["DATABASE_USER"],
-            "password" to props["DATABASE_PASSWORD"],
-            "count" to 1,
-            "logLevel" to "debug"
-        )
+        logger.info("########################################################")
+        logger.info("## activities.register = ${liquibase.activities.names}")
+        logger.info("########################################################")
     }
     runList = "update"
 }
 
+tasks.named<LiquibaseTask>("update") {
+    doFirst {
+        liquibase.setProperty("runList", "update")
+        logger.info("########################################################")
+        logger.info("## doFirst ${liquibase.activities.asMap}")
+        logger.info("########################################################")
+    }
+
+    logger.info("########################################################")
+    logger.info("## named = ${liquibase.activities.names}")
+    logger.info("## activities[update] = ${liquibase.activities.getByName("update")}")
+    logger.info("## activities[update].args = ${liquibase.activities.getByName("update").arguments}")
+    logger.info("########################################################")
+}
